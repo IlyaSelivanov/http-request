b1928c1f3b147bf73afb084a2032bc7876a5cf3f
IlyaSelivanov <selivanov.ilya@gmail.com>
2023-10-11 15:33:11 +0300

    simple request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+static URL: &str = "";
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let response = reqwest::get(URL)
+        .await?;
+
+    println!("{:#?}", response.text().await?);
+
+    Ok(())
 }
