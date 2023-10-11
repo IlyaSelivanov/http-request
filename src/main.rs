2e009894bd9b36caa5bed20abf213b75dfaefee7
IlyaSelivanov <selivanov.ilya@gmail.com>
2023-10-11 16:16:05 +0300

    clap cli

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,43 @@
 use std::error::Error;
 
-static URL: &str = "";
+use clap::{Parser, command, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Get {
+        #[arg(short, long)]
+        url: String
+    },
+}
+
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let response = reqwest::get(URL)
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Get { url }) => {
+            send_get(url).await?
+        }
+        None => {}
+    }
+    
+    Ok(())
+}
+
+async fn send_get(url: &str) -> Result<(), Box<dyn Error>> {
+    let response = reqwest::get(url)
         .await?;
 
-    println!("{:#?}", response.text().await?);
+    println!("{:#?}", response.status());
 
     Ok(())
 }
