d1b6c0b961b179847feb1769b126991f82534dda
IlyaSelivanov <selivanov.ilya@gmail.com>
2023-10-17 17:25:30 +0300

    cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,33 @@
 use std::error::Error;
 
-use clap::{Parser, command, Subcommand};
+use clap::{command, Parser};
 
 #[derive(Parser)]
-#[command(author, version, about, long_about = None)]
+#[command(name = "http-request")]
+#[command(author = "Ilya S. <selivanov.ilya@gmail.com>")]
+#[command(version = "0.1.0")]
+#[command(about = "Sends http request", long_about = None)]
 struct Cli {
-    #[command(subcommand)]
-    command: Option<Commands>,
+    #[arg(short, long)]
+    url: String,
+    #[arg(short, long)]
+    method: String,
 }
-
-#[derive(Subcommand)]
-enum Commands {
-    Get {
-        #[arg(short, long)]
-        url: String
-    },
-}
-
-
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
 
-    match &cli.command {
-        Some(Commands::Get { url }) => {
-            send_get(url).await?
-        }
-        None => {}
+    match cli.method.as_str() {
+        "get" => send_get(&cli.url).await.unwrap(),
+        _ => {}
     }
-    
+
     Ok(())
 }
 
 async fn send_get(url: &str) -> Result<(), Box<dyn Error>> {
-    let response = reqwest::get(url)
-        .await?;
+    let response = reqwest::get(url).await?;
 
     println!("{:#?}", response.status());
 
