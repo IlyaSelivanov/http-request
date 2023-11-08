d5c090a581094a40fdfe05cba0f39485752340fc
Ilya S <selivanov.ilya@gmail.com>
2023-11-08 17:06:35 +0200

    Add event handling to Component trait

--- src/components.rs
+++ src/components.rs
@@ -1,39 +1,45 @@
 use anyhow::Error;
 use crossterm::event::{KeyEvent, MouseEvent};
 use ratatui::{prelude::Rect, Frame};
+use tokio::sync::mpsc::UnboundedSender;
 
 use crate::{action::Action, ui::Event};
 
+mod url;
+
 pub trait Component {
+    #[allow(unused_variables)]
+    fn register_action_handler(&mut self, tx: UnboundedSender<Action>) -> Result<(), Error> {
+        Ok(())
+    }
+
     fn init(&mut self) -> Result<(), Error> {
         Ok(())
     }
 
-    fn handle_event(&mut self, event: Option<Event>) -> Action {
-        match event {
-            Some(Event::Quit) => Action::Quit,
-            Some(Event::Tick) => Action::Tick,
-            Some(Event::Render) => Action::Render,
-            Some(Event::Key(key)) => self.handle_key(key),
-            Some(Event::Mouse(mouse)) => self.handle_mouse(mouse),
-            _ => Action::None,
-        }
+    fn handle_events(&mut self, event: Option<Event>) -> Result<Option<Action>, Error> {
+        let r = match event {
+            Some(Event::Key(key_event)) => self.handle_key_events(key_event)?,
+            Some(Event::Mouse(mouse_event)) => self.handle_mouse_events(mouse_event)?,
+            _ => None,
+        };
+        Ok(r)
     }
 
     #[allow(unused_variables)]
-    fn handle_key(&mut self, key: KeyEvent) -> Action {
-        Action::None
+    fn handle_key_events(&mut self, key: KeyEvent) -> Result<Option<Action>, Error> {
+        Ok(None)
     }
 
     #[allow(unused_variables)]
-    fn handle_mouse(&mut self, mouse: MouseEvent) -> Action {
-        Action::None
+    fn handle_mouse_events(&mut self, mouse: MouseEvent) -> Result<Option<Action>, Error> {
+        Ok(None)
     }
 
     #[allow(unused_variables)]
-    fn update(&mut self, action: Action) -> Action {
-        Action::None
+    fn update(&mut self, action: Action) -> Result<Option<Action>, Error> {
+        Ok(None)
     }
 
-    fn render(&mut self, f: &mut Frame<'_>, rect: Rect);
+    fn render(&mut self, f: &mut Frame<'_>, rect: Rect) -> Result<(), Error>;
 }
